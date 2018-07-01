c701a2d060aed062430690f3640778d990c8a76d
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-07-01 15:33:21 -0300

    :pencil: | Adicionar mais funcionalidades ao "/dreamcore"

--- src/main/java/net/perfectdreams/dreamcore/DreamCore.kt
+++ src/main/java/net/perfectdreams/dreamcore/DreamCore.kt
@@ -49,8 +49,11 @@ class DreamCore : JavaPlugin() {
 					return true
 				}
 
-				if (p3.isNotEmpty() && p0 is Player) {
-					if (p3[0] == "set_spawn") {
+				val arg0 = p3.getOrNull(0)
+				val arg1 = p3.getOrNull(1)
+
+				if (p0 is Player) {
+					if (arg0 == "set_spawn") {
 						dreamConfig.spawn = p0.location
 
 						configFile.writeText(DreamUtils.gson.toJson(dreamConfig))
@@ -60,7 +63,69 @@ class DreamCore : JavaPlugin() {
 					}
 				}
 
+				if (arg0 == "softreload" && arg1 != null) {
+					val plugin = Bukkit.getPluginManager().getPlugin(arg1)
+
+					if (plugin == null) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não existe! Talvez você queria usar §6/plugman reload§c?")
+						return true
+					}
+
+					if (plugin !is KotlinPlugin) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não extende KotlinPlugin!")
+						return true
+					}
+
+					p0.sendMessage("§7\"Soft\" desativando §e${plugin.name}§7...")
+					plugin.softDisable()
+					p0.sendMessage("§7\"Soft\" ativando §e${plugin.name}§7...")
+					plugin.softEnable()
+					p0.sendMessage("§e${plugin.name}§a foi \"soft\" recarregado com sucesso!")
+					return true
+				}
+
+				if (arg0 == "load" && arg1 != null) {
+					val plugin = Bukkit.getPluginManager().getPlugin(arg1)
+
+					if (plugin == null) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não existe! Talvez você queria usar §6/plugman load§c?")
+						return true
+					}
+
+					if (plugin !is KotlinPlugin) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não extende KotlinPlugin!")
+						return true
+					}
+
+					p0.sendMessage("§7\"Soft\" ativando §e${plugin.name}§7...")
+					plugin.softEnable()
+					p0.sendMessage("§e${plugin.name}§a foi \"soft\" ativado com sucesso!")
+					return true
+				}
+
+				if (arg0 == "unload" && arg1 != null) {
+					val plugin = Bukkit.getPluginManager().getPlugin(arg1)
+
+					if (plugin == null) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não existe! Talvez você queria usar §6/plugman unload§c?")
+						return true
+					}
+
+					if (plugin !is KotlinPlugin) {
+						p0.sendMessage("§cPlugin §e${arg1}§c não extende KotlinPlugin!")
+						return true
+					}
+
+					p0.sendMessage("§7\"Soft\" dtivando §e${plugin.name}§7...")
+					plugin.softEnable()
+					p0.sendMessage("§e${plugin.name}§a foi \"soft\" ativado com sucesso!")
+					return true
+				}
+
 				p0.sendMessage("§e/dreamcore set_spawn")
+				p0.sendMessage("§e/dreamcore softreload")
+				p0.sendMessage("§e/dreamcore softload")
+				p0.sendMessage("§e/dreamcore softunload")
 				return true
 			}
 		}.register()
