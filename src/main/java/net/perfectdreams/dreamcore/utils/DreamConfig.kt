30ed876bb7cf9906e329322d7d297ff7474ffd1b
MrPowerGamerBR <leonardomalaman@gmail.com>
2018-10-01 19:23:47 -0300

    Carregar as configurações do PostgreSQL da config.yml

--- src/main/java/net/perfectdreams/dreamcore/utils/DreamConfig.kt
+++ src/main/java/net/perfectdreams/dreamcore/utils/DreamConfig.kt
@@ -11,7 +11,7 @@ class DreamConfig(val serverName: String, val bungeeName: String) {
 	var serverDatabaseName = "perfectdreams_survival"
 	var mongoDbIp = "10.0.0.3"
 	var postgreSqlIp = "10.0.0.6"
-	var postgreSqlPort = 3306
+	var postgreSqlPort = 5432
 	lateinit var postgreSqlUser: String
 	lateinit var postgreSqlPassword: String
 	lateinit var spawn: Location
